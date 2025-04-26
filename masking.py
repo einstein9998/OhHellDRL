final fixes to dict observation - now action masking works correctly

masking.py
@@ -1,17 +1,15 @@
-from stable_baselines3.ppo.policies import MlpPolicy
-from torch import nn
+from stable_baselines3.ppo.policies import ActorCriticPolicy
 import torch
 
-class MaskedMlpPolicy(MlpPolicy):
-    def forward(self, obs_dict, deterministic=False):
-        obs = obs_dict["obs"]
-        mask = obs_dict["action_mask"]
+class MaskedMlpPolicy(ActorCriticPolicy):
+    def forward(self, obs, deterministic=False):
+        mask = obs["action_mask"]
         features = self.extract_features(obs)
         latent_pi, latent_vf = self.mlp_extractor(features)
         distribution = self._get_action_dist_from_latent(latent_pi)
 
         logits = distribution.distribution.logits
-        masked_logits = logits + (1 - torch.tensor(mask)) * -1e9
+        masked_logits = logits + (1 - mask) * -1e9
         distribution.distribution = torch.distributions.Categorical(logits=masked_logits)
 
         actions = distribution.get_actions(deterministic=deterministic)